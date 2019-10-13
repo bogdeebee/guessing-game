891f3bb867b4842d8181899828b7f0e5b8441992
Olga Bogdanova <bogdeebee@gmail.com>
2019-10-14 02:31:02 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.middleNumber = Math.round((this.min + this.max) / 2);
 
+        return this.middleNumber;
     }
 
     lower() {
-
+        this.max = this.middleNumber;
     }
 
     greater() {
-
+        this.min = this.middleNumber;
     }
 }
 
